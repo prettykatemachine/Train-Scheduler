bd68ba43a8108a960de87a11f9fea0c1a99868c9
unknown <katie.albee@gmail.com>
2018-04-23 18:15:14 -0500

    Initializing Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
 //To Do:
-//Initialize Firebase
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDt485IpaMqi51YqWDKe1fNXxTcYoaCLao",
+    authDomain: "train-scheduler-df993.firebaseapp.com",
+    databaseURL: "https://train-scheduler-df993.firebaseio.com",
+    projectId: "train-scheduler-df993",
+    storageBucket: "train-scheduler-df993.appspot.com",
+    messagingSenderId: "388090364657"
+  };
+  firebase.initializeApp(config);
+
 //Display Current Time
 //Button for Adding Trains
 //It should grab the user input
