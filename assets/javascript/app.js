2ed56f3da1ba92b564726e70fd3db59f3656a32f
unknown <katie.albee@gmail.com>
2018-04-23 15:55:05 -0500

    Today was thankfully a slow day at work.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//To Do:
+//Initialize Firebase
+//Display Current Time
+//Button for Adding Trains
+//It should grab the user input
+//Use local Storage for Train Data
+//Upload the Train Data to Firebase
+//Add an HTML row of the train
+//Timing... Calculate Time Between Train and Now (train arrival)
+//Displaying train information in the table
+//Update schedule every ... 30 seconds?  Minute?
